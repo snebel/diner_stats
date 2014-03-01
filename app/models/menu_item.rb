3ef22963b7ae137e9db3893f0351eac863615d29
snebel <sam@samnebel.com>
2014-03-01 12:31:04 -0500

    initial tables view

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -3,4 +3,8 @@ class MenuItem < ActiveRecord::Base
   has_many :meals, through: :meal_memberships
   belongs_to :section
 
+  def to_s
+    "#{self.name} $#{self.price}"
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ActiveRecord::Base
   has_many :tables
   has_many :menus
+  has_many :menu_items, through: :menus
   has_many :meals, through: :tables
 end
