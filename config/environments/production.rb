aed0de7cabbcb534f6608b62de9b86d157924985
snebel <sam@samnebel.com>
2014-03-04 21:07:49 -0500

    compile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 DinerStats::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.assets.compile = true
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -20,14 +20,14 @@ DinerStats::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
